be1416e1bedc8cf87a40a9b069961cfa3d5e16e4
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-03 17:24:40 +0200

    script to draw player

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
+import { Player } from "./classes/Player.js";
+
 const canvas = document.getElementById("game-container");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
+
+
+const player = new Player(canvas.width / 2, canvas.height /2, 10, "red");
+player.draw(ctx);

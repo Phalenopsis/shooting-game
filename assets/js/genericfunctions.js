dcb4adeb33ead196e911677c48791d839a518e6c
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-04 13:39:28 +0200

    add get ennemyOrigin

--- assets/js/genericfunctions.js
+++ assets/js/genericfunctions.js
@@ -15,3 +15,27 @@ export function getRandomHslColor() {
 
     return `hsl(${hue} ${saturation}% ${luminosity}%)`
 }
+
+export function getEnnemyOrigin() {
+    const maxWidth = window.innerWidth;
+    const maxHeight = window.innerHeight;
+    const dirInt = getRandomInt(4);
+    switch (dirInt) {
+        case 0:
+            return { x: 0, y: getRandomInt(maxHeight) };
+            break;
+        case 1:
+            return { x: maxWidth, y: getRandomInt(maxHeight) };
+            break;
+        case 2:
+            return { x: getRandomInt(maxWidth), y: 0 };
+            break;
+        case 3:
+            return { x: getRandomInt(maxWidth), y: maxHeight };
+            break;
+
+        default:
+            throw new Error("Le nombre de getEnnemyOrigin est trop grand ou trop petit");
+            break;
+    }
+}

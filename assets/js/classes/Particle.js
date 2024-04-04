56fe0a1049cc1002183a95dc30eee43a23b899b3
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-04 16:56:49 +0200

    add Particle class

---
+++ assets/js/classes/Particle.js
@@ -0,0 +1,25 @@
+import { Enemy } from "./Enemy.js";
+
+export class Particle extends Enemy {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color, velocity);
+        this.alpha = 1;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.pi * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.restore();
+    }
+
+    update(ctx) {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.alpha -= 0.01;
+    }
+}

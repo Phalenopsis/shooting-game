667da6c8d09d3722d58ffa9867fe61ddc2f81dfc
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-04 08:30:14 +0200

    add class Projectile and create them in index.js

---
+++ assets/js/classes/Projectile.js
@@ -0,0 +1,8 @@
+import { Player } from "./Player.js";
+
+export class Projectile extends Player {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color);
+        this.velocity = velocity;
+    }
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 import { Player } from "./classes/Player.js";
+import {Projectile} from "./classes/Projectile.js";
 
 const canvas = document.getElementById("game-container");
 canvas.width = window.innerWidth;
@@ -9,3 +10,13 @@ var ctx = canvas.getContext('2d');
 
 const player = new Player(canvas.width / 2, canvas.height /2, 10, "red");
 player.draw(ctx);
+const projectile = new Projectile(50, 50, 30, "blue", {x:3, y:3});
+projectile.draw(ctx);
+
+const projectiles = [];
+
+window.addEventListener("click", (e) => {
+    const projectile = new Projectile(e.clientX, e.clientY, 5, "white", null);
+    projectile.draw(ctx);
+    projectiles.push(projectile);
+});

8efa0c664853e195e2005caaf56d32acd56d0f80
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-04 12:00:24 +0200

    change randomInt to use it with 1 or 2 args

--- assets/js/genericfunctions.js
+++ assets/js/genericfunctions.js
@@ -1,3 +1,9 @@
-export function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+export function getRandomInt(value1, value2 = undefined) {
+    if (value2 === undefined) {
+        const max = value1;
+        return Math.floor(Math.random() * max);
+    }
+    const min = Math.ceil(value1);
+    const max = Math.floor(value2);
+    return Math.floor(Math.random() * (max - min)) + min;
 }

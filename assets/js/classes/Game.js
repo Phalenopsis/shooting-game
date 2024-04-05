127855e1d14ef035f841ab9e880753cead4147f1
Nicolas Leroux <hertford.fr@gmail.com>
2024-04-05 10:39:50 +0200

    add a global class Game

---
+++ assets/js/classes/Game.js
@@ -0,0 +1,10 @@
+export class Game {
+    constructor(player, canvas) {
+        this.player = player;
+        //this.canvas = canvas;
+        //this.ctx = ctx;
+        this.enemies = [];
+        this.projectiles = [];
+        this.particles = [];
+    }
+}
